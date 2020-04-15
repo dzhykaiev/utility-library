203f5202a5d3e85b99061866609d178238d075cc
dzhykaiev <yevhenii.dzhykaiev@gmail.com>
2020-04-15 23:33:07 +0300

    added methods - (total:2) : - drop - dropRight

---
+++ src/array.ts
@@ -0,0 +1,52 @@
+export const chunk = function <T>(array: T[], size: number): T[] {
+  const s = size || 1;
+  let result = new Array();
+
+  for (let i = 0; i < array.length; i++) {
+    if (i % s === 0) {
+      result = [...result, array.slice(i, i + s)];
+    }
+  }
+
+  return result;
+};
+
+export const compact = function <T>(array: T[]): T[] {
+  return array.filter((item) => {
+    return Boolean(item);
+  });
+};
+
+export const concat = function <T, U extends any[]>(array: T[], ...values: U) {
+  return values.reduce((acc, cur) => {
+    return acc.concat(cur);
+  }, array);
+};
+
+export const difference = function <T>(array: T[], values: T[]) {
+  return array.filter((element) => !values.includes(element));
+};
+
+export const differenceBy = function <T>(
+  array: T[],
+  values: T[],
+  iteratee: any
+) {
+  return array.filter((element) => {
+    return values.every((value) => {
+      return iteratee(value) != iteratee(element);
+    });
+  });
+};
+
+// TODO: add differenceWith method
+
+export const drop = <T>(array: T[], n: number = 1) => {
+  array.splice(0, n);
+  return array;
+};
+
+export const dropRight = <T>(array: T[], n: number = 1) => {
+  n ? array.splice(-n) : null;
+  return array;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,54 @@
-export const chunk = function <T>(array: T[], size: number): T[] {
-  const s = size || 1;
-  let result = new Array();
+import {
+  chunk,
+  compact,
+  concat,
+  difference,
+  differenceBy,
+  drop,
+  dropRight,
+} from "./array";
 
-  for (let i = 0; i < array.length; i++) {
-    if (i % s === 0) {
-      result = [...result, array.slice(i, i + s)];
-    }
-  }
-
-  return result;
-};
-
+/*
+ ** chunk
+ */
 // console.log(chunk(["a", "b", "c", "d"], 2)); // => [['a', 'b'], ['c', 'd']]
 // console.log(chunk(["a", "b", "c", "d"], 3)); // => [['a', 'b', 'c'], ['d']]
 
-export const compact = function <T>(array: T[]): T[] {
-  return array.filter((item) => {
-    return Boolean(item);
-  });
-};
-
+/*
+ ** compact
+ */
 // console.log(compact([0, 1, false, 2, "", 3])); // => [1, 2, 3]
 
-export const concat = function <T, U extends any[]>(array: T[], ...values: U) {
-  return values.reduce((acc, cur) => {
-    return acc.concat(cur);
-  }, array);
-};
-
+/*
+ ** concat
+ */
 // var array = [1];
 // var other = concat(array, 2, [3], [[4]]);
 // console.log(other); // => [1, 2, 3, [4]]
 // console.log(array); // => [1]
 
-export const difference = function <T>(array: T[], values: T[]) {
-  return array.filter((element) => !values.includes(element));
-};
-
+/*
+ ** difference
+ */
 // console.log(difference([2, 1], [2, 3])); // => [1]
 
-export const differenceBy = function <T>(
-  array: T[],
-  values: T[],
-  iteratee: any
-) {
-  return array.filter((element) => {
-    return values.every((value) => {
-      return iteratee(value) != iteratee(element);
-    });
-  });
-};
-
+/*
+ ** differenceBy
+ */
 // console.log(differenceBy([2.1, 1.2], [2.3, 3.4], Math.floor)); // => [1.2]
+
+/*
+ ** drop
+ */
+// console.log(drop([1, 2, 3])); // => [2, 3]
+// console.log(drop([1, 2, 3], 2)); // => [3]
+// console.log(drop([1, 2, 3], 5)); // => []
+// console.log(drop([1, 2, 3], 0)); // => [1, 2, 3]
+
+/*
+ ** dropRight
+ */
+console.log(dropRight([1, 2, 3])); // => [1, 2]
+console.log(dropRight([1, 2, 3], 2)); // => [1]
+console.log(dropRight([1, 2, 3], 5)); // => []
+console.log(dropRight([1, 2, 3], 0)); // => [1, 2, 3]
